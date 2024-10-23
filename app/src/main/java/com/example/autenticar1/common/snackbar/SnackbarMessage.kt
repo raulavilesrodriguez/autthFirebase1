c983d706969388a53170c922478ce86e40addf38
raulavilesrodriguez <finbravilesr@gmail.com>
2024-10-23 12:53:04 -0500

    code to SnackbarMessage

--- app/src/main/java/com/example/autenticar1/common/snackbar/SnackbarMessage.kt
+++ app/src/main/java/com/example/autenticar1/common/snackbar/SnackbarMessage.kt
@@ -1,4 +1,25 @@
 package com.example.autenticar1.common.snackbar
 
-class SnackbarMessage {
+import android.content.res.Resources
+import androidx.annotation.StringRes
+import com.example.autenticar1.R.string as AppText
+
+sealed class SnackbarMessage {
+    class StringSnackbar(val message: String) : SnackbarMessage()
+    class ResourceSnackbar(@StringRes val message: Int) : SnackbarMessage()
+
+    companion object {
+        fun SnackbarMessage.toMessage(resources: Resources): String {
+            return when (this) {
+                is StringSnackbar -> this.message
+                is ResourceSnackbar -> resources.getString(this.message)
+            }
+        }
+
+        fun Throwable.toSnackbarMessage(): SnackbarMessage {
+            val message = this.message.orEmpty()
+            return if (message.isNotBlank()) StringSnackbar(message)
+            else ResourceSnackbar(AppText.generic_error)
+        }
+    }
 }
