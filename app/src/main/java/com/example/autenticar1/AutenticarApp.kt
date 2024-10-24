dddccc73fc2467285b57dfa0cb07d0504bc592d0
raulavilesrodriguez <finbravilesr@gmail.com>
2024-10-23 19:08:37 -0500

    working in navigation

--- app/src/main/java/com/example/autenticar1/AutenticarApp.kt
+++ app/src/main/java/com/example/autenticar1/AutenticarApp.kt
@@ -1,18 +1,36 @@
 package com.example.autenticar1
 
 import android.Manifest
-import android.os.Build
-import androidx.annotation.RequiresApi
+import android.content.res.Resources
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.autenticar1.common.composable.PermissionDialog
 import com.example.autenticar1.common.composable.RationaleDialog
+import com.example.autenticar1.common.snackbar.SnackbarManager
 import com.example.autenticar1.ui.theme.Autenticar1Theme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.permissions.shouldShowRationale
+import kotlinx.coroutines.CoroutineScope
 
 @Composable
 fun AutenticarApp(){
@@ -20,7 +38,27 @@ fun AutenticarApp(){
         RequestNotificationPermissionDialog()
 
         Surface(color = MaterialTheme.colorScheme.background) {
+            val appState = rememberAppState()
 
+            Scaffold(
+                snackbarHost = {
+                    SnackbarHost(
+                        hostState = appState.snackbarHostState,
+                        modifier = Modifier.padding(8.dp),
+                        snackbar = { snackbarData ->
+                            Snackbar(snackbarData, contentColor = MaterialTheme.colorScheme.onPrimary)
+                        }
+                    )
+                },
+            ) { innerPaddingModifier ->
+                NavHost(
+                    navController = appState.navController,
+                    startDestination = SPLASH_SCREEN,
+                    modifier = Modifier.padding(innerPaddingModifier)
+                ) {
+                    autenticarGraph(appState)
+                }
+            }
         }
 
     }
@@ -39,3 +77,29 @@ fun RequestNotificationPermissionDialog() {
 
 }
 
+@Composable
+fun rememberAppState(
+    snackbarHostState: SnackbarHostState = remember { SnackbarHostState() },
+    navController: NavHostController = rememberNavController(),
+    snackbarManager: SnackbarManager = SnackbarManager,
+    resources: Resources = resources(),
+    coroutineScope: CoroutineScope = rememberCoroutineScope()
+) =
+    remember(snackbarHostState, navController, snackbarManager, resources,coroutineScope){
+        AutenticarAppState(snackbarHostState, navController, snackbarManager, resources, coroutineScope)
+    }
+
+
+@Composable
+@ReadOnlyComposable
+fun resources(): Resources {
+    LocalConfiguration.current
+    return LocalContext.current.resources
+}
+
+
+fun NavGraphBuilder.autenticarGraph(appState: AutenticarAppState){
+    composable(SPLASH_SCREEN){
+
+    }
+}
